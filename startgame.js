3e3d79951863eb13f68faf754826e0aab8010e3e
NadiaBCarmon <nadia.prysmmagazine@gmail.com>
2018-03-30 17:37:01 -0400

    Created boilerplate ajax request and a class Pokemon with a constructor

--- startgame.js
+++ startgame.js
@@ -1,5 +1,34 @@
  $('#on-button').click(function(){
-      $('#welcome').css("visibility", "visible")
-
-      
+      $('#welcome').css("visibility", "visible")     
 })
+
+//  class Pokemon {
+
+// 	constructor(hp, attack, defense, abilities){
+// 	this.hp = hp(pokemon);
+// 	this.attack = attack(pokemon);
+// 	this.defense = defense(pokemon);
+// 	this.abilities = abilities(pokemon);
+// 	}
+// }
+
+  $('#on-button').click(function(){
+      $('#welcome').css("visibility", "visible")     
+})
+
+  $.ajax({
+
+	  	type: "GET",
+
+	  	url: "http://pokeapi.co/api/v2/pokemon/kakuna",
+
+		  	success: function(kakuna){
+		  		console.log(kakuna)
+		  	},
+
+		  	error: function(){
+
+		  	}
+
+
+  })//end of ajax request//
